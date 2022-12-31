8d63758b5850dea2fac25e582471a4f89d06fbef
betty <saviobetty@gmail.com>
2022-12-31 15:06:00 +1100

    Minor Change to PriceClient.java

--- P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO PRICE (currency, price, vehicleId) VALUES ('USD', 4000, 1);
-INSERT INTO PRICE (currency, price, vehicleId)  VALUES ('USD', 5000, 2);
-INSERT INTO PRICE (currency, price, vehicleId)  VALUES ('USD', 6000, 3);
-INSERT INTO PRICE (currency, price, vehicleId)  VALUES ('USD', 7000, 4);
+INSERT INTO PRICE (currency, price, id) VALUES ('USD', 4000, 1);
+INSERT INTO PRICE (currency, price, id)  VALUES ('USD', 5000, 2);
+INSERT INTO PRICE (currency, price, id)  VALUES ('USD', 6000, 3);
+INSERT INTO PRICE (currency, price, id)  VALUES ('USD', 7000, 4);
